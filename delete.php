0ed2e7fcad71b8e2fb3dd1d731b2ddd64b98f08b
DwiAsfi <dwiasfi2@gmail.com>
2022-04-01 22:45:50 +0700

    Menambahkan file delete.php

---
+++ delete.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  	<head>
+    	<!-- Required meta tags -->
+    	<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    	<!-- Bootstrap CSS -->
+    	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    	<title>Delete</title>
+  	</head>
+  	<body>
+  		<div class="container" align="center">
+		    <?php 
+			include "koneksi.php";
+
+			$id = $_GET["id"];
+			
+			$sql = "DELETE FROM tbl_121 WHERE kode_buku = '$id'";
+			$hasil = mysqli_query($koneksi,$sql);
+			if (!$hasil) { ?>
+				<h2>Eksekusi Gagal</h2>
+				<?php }
+				else { ?>
+					<h2 class="mt-3">Data Telah Berhasil Dihapus<br></h2>
+					<a href="dataBuku.php" button type="button" class="btn btn-success mt-3">Lihat Daftar Buku</a>
+					<?php }
+					?>
+		</div>
+  	</body>
+</html>
